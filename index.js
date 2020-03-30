e5c4a62b5be1cf5d031a4bcdae6060789f0689bb
Chris Ramirez <chris.ramirez163@gmail.com>
2020-03-30 00:10:54 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+function getDogPicture() {
+  const dogBreed = document.getElementById("breed").value;
+  fetch(`https://dog.ceo/api/breed/${dogBreed}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => {
+      if (responseJson.status === "error") {
+        alert(responseJson.message);
+      } else {
+        displayResults(responseJson);
+        alert("You know your breeds, here is a cute picture");
+      }
+    })
+    .catch(error => alert("Something went wrong"));
+}
+
+function displayResults(responseJson) {
+  const imageUrls = responseJson.message;
+  $(".images-container").replaceWith(
+    `<img src="${imageUrls}" class="results-img">`
+  );
+  $(".results").removeClass("hidden");
+}
+
+function handleForm() {
+  $("form").submit(event => {
+    event.preventDefault();
+    getDogPicture();
+  });
+}
+
+$(function() {
+  console.log("Lets go!");
+  handleForm();
+});
